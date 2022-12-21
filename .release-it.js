7db631a6dd5528b703de1021e6d1eccf083c3f0d
Mykhailo Onikiienko <michael.onikienko@gmail.com>
2022-12-21 20:48:07 +0200

    feat(release-it): publish release to GitHub

--- .release-it.js
+++ .release-it.js
@@ -1,24 +1,27 @@
 const {extName} = require('./utils/getExtName.js');
 
-console.log(extName);
 
 module.exports = {
     'git': {
-        requireCleanWorkingDir: false,
+        'requireCleanWorkingDir': true,
+        'commitMessage': 'v${version}',
+        'pushRepo': 'origin',
+        'tagName': 'v${version}',
+        'requireCommits': true,
     },
     'github': {
-        'release': false,
-        // 'releaseName': 'v${version}',
-        // 'tokenRef': 'GITHUB_TOKEN',
-        // 'assets': `releases/${extName}` + '-v${version}.zip',
+        'release': true,
+        'releaseName': 'v${version}',
+        'tokenRef': 'GITHUB_TOKEN',
+        'assets': `releases/${extName}` + '-v${version}.zip',
     },
     'npm': {
         'publish': false,
     },
     'hooks': {
+        // bump version in the manifest.json to be the same as in the package.json
         'before:bump': ['dot-json src/manifest.json version "${version}"'],
         'after:bump': ['npm run build'],
-        // 'after:git:release': 'echo Stage: After git push, before github release;',
         'after:release': [
             'echo Successfully released ${name} v${version} to ${repo.repository}',
             'opener https://${repo.host}/${repo.repository}/releases/tag/v${version}',
