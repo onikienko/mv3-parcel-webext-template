0c6e140151604ffc48ea05141023f09ba3f9c244
Mykhailo Onikiienko <michael.onikienko@gmail.com>
2022-12-21 18:03:44 +0200

    wip(release-it): update config

--- .release-it.js
+++ .release-it.js
@@ -3,13 +3,25 @@ const {extName} = require('./utils/getExtName.js');
 console.log(extName);
 
 module.exports = {
+    'git': {
+        requireCleanWorkingDir: false,
+    },
     'github': {
-        'release': true,
-        'releaseName': 'v${version}',
+        'release': false,
+        // 'releaseName': 'v${version}',
         // 'tokenRef': 'GITHUB_TOKEN',
-        'assets': `releases/${extName}` + '-v${version}.zip',
+        // 'assets': `releases/${extName}` + '-v${version}.zip',
     },
     'npm': {
         'publish': false,
+    },
+    'hooks': {
+        'before:bump': ['dot-json src/manifest.json version "${version}"'],
+        'after:bump': ['npm run build'],
+        // 'after:git:release': 'echo Stage: After git push, before github release;',
+        'after:release': [
+            'echo Successfully released ${name} v${version} to ${repo.repository}',
+            'opener https://${repo.host}/${repo.repository}/releases/tag/v${version}',
+        ],
     },
 };
