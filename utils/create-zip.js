370300f3a27601374bff49bb3b8788917e1ee5d3
Mykhailo Onikiienko <michael.onikienko@gmail.com>
2022-12-22 14:40:00 +0200

    fix: use version from package.json for zip and remove unneeded getExtName.js

--- utils/create-zip.js
+++ utils/create-zip.js
@@ -1,6 +1,6 @@
+const filenamify = require('filenamify');
 const fs = require('fs-extra');
 const path = require('node:path');
-const extName = require('./getExtName');
 const zl = require('zip-lib');
 
 const DIST_DIR = './dist';
@@ -8,11 +8,11 @@ const RELEASE_DIR = './releases';
 
 if (!fs.pathExistsSync(DIST_DIR)) throw new Error('Dist dir does not exist');
 
-const {version} = fs.readJsonSync('./src/manifest.json');
+const {name, version} = fs.readJsonSync('./package.json');
+const extName = filenamify(name, {replacement: '_'});
 const zipName = `${extName}-v${version}.zip`;
-
 const zipPath = path.join(RELEASE_DIR, zipName);
 
 zl.archiveFolder(DIST_DIR, zipPath)
-        .then(() => console.log(`Release zip created - ${zipPath}`))
-        .catch(console.log);
+    .then(() => console.log(`Release zip created - ${zipPath}`))
+    .catch(console.log);
--- utils/getExtName.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs-extra');
-const filenamify = require('filenamify');
-
-const {name} = fs.readJsonSync('./package.json');
-const extName = filenamify(name, {replacement: '_'});
-module.exports = extName;
