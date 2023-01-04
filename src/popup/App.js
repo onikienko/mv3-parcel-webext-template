f7c5a78180aaec5f62e00b62ebcc32bc88725a64
Mykhailo Onikiienko <michael.onikienko@gmail.com>
2023-01-04 13:54:59 +0200

    chore(popup): add extension version number to the window

--- src/popup/App.js
+++ src/popup/App.js
@@ -6,6 +6,7 @@ export const App = () => {
         <>
             <img src={logo} alt="Logo"/>
             <h1>Popup Window</h1>
+            <div>v{chrome.runtime.getManifest().version}</div>
         </>
     );
 };
