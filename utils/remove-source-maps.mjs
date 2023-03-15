4816ca3ae3e7b6c244227b89f896e658c7b999ae
Mykhailo Onikiienko <michael.onikienko@gmail.com>
2023-03-15 17:54:38 +0200

    build: upgrade glob to v9.3.0. and adapt remove-source-maps.mjs

--- utils/remove-source-maps.mjs
+++ utils/remove-source-maps.mjs
@@ -1,17 +1,15 @@
-import glob from 'glob';
+import {glob} from 'glob';
 import {readFileSync, unlinkSync, writeFileSync} from 'node:fs';
 
 
-glob('dist/**/*.map', (err, matches) => {
-    if (err) throw err;
-    matches.forEach(file => unlinkSync(file));
-});
+(async () => {
+    const mapFiles = await glob('dist/**/*.map');
+    mapFiles.forEach(file => unlinkSync(file));
 
-glob('dist/**/*.+(css|js)', (err, matches) => {
-    if (err) throw err;
-    matches.forEach(file => {
+    const files = await glob('dist/**/*.+(css|js)');
+    files.forEach(file => {
         const data = readFileSync(file, {encoding: 'utf8'});
         const res = data.replace(/\/[*|\/]# sourceMappingURL=.+\n/, '');
         writeFileSync(file, res);
     });
-});
+})();
