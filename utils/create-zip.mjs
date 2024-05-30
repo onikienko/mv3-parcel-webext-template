621bd5da031b4bc70f3358f40701188ca66ca8f9
Mykhailo Onikiienko <michael.onikienko@gmail.com>
2024-05-29 17:54:42 -0700

    build: do not use fs-extra for a create-zip script

--- utils/create-zip.mjs
+++ utils/create-zip.mjs
@@ -1,5 +1,5 @@
 import filenamify from 'filenamify';
-import fs from 'fs-extra';
+import {readFileSync} from 'node:fs';
 import path from 'node:path';
 import zl from 'zip-lib';
 
@@ -7,13 +7,10 @@ import zl from 'zip-lib';
 const DIST_DIR = './dist';
 const RELEASE_DIR = './releases';
 
-if (!fs.pathExistsSync(DIST_DIR)) throw new Error('Dist dir does not exist');
-
-const {name, version} = fs.readJsonSync('./package.json');
+const {name, version} = JSON.parse(readFileSync('./package.json', {encoding: 'utf8'}));
 const extName = filenamify(name, {replacement: '_'});
 const zipName = `${extName}-v${version}.zip`;
 const zipPath = path.join(RELEASE_DIR, zipName);
 
 zl.archiveFolder(DIST_DIR, zipPath)
-    .then(() => console.log(`Release zip created - ${zipPath}`))
-    .catch(console.log);
+        .then(() => console.log(`Release zip created - ${zipPath}`));
